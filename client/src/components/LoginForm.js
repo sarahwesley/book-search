64ee2d7fc17cd649ba8bc53f2ca8551a9b853792
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2022-03-20 10:57:50 -0400

    fixing issues

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -19,12 +19,6 @@ const LoginForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    
-    const form = event.currentTarget;
-    if(form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
     try {
       const { data } = await login({
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -22,11 +22,6 @@ const SignupForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation;
-    }
     // refactored code
     try {
       const { data } = await addUser({
